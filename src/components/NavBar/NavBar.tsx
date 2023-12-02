bb4d1e2b59969daa4bc500e3ec73ea373a5ae868
WasthElse2002 <alexandreredon.pro@gmail.com>
2023-12-02 18:57:57 +0100

    fix: menu responsive

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,13 @@
 import "./navbar.css";
 import logo from "../../assets/logo.png";
 import contact from "../../assets/contact.svg";
+import menu from "../../assets/menu.png";
 import { Link } from "react-scroll";
+import { useState } from "react";
 
 function NavBar() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <nav className="navBar">
       <a href="/">
@@ -11,10 +15,12 @@ function NavBar() {
       </a>
       <div className="desktopMenu">
         <Link
+          activeClass="active"
           className="desktopMenuItemListItem"
           to="presentation"
           spy={true}
           smooth={true}
+          offset={-100}
         >
           Accueil
         </Link>
@@ -23,6 +29,7 @@ function NavBar() {
           to="a-propos"
           spy={true}
           smooth={true}
+          offset={-100}
         >
           À propos
         </Link>
@@ -31,6 +38,7 @@ function NavBar() {
           to="portfolio"
           spy={true}
           smooth={true}
+          offset={-100}
         >
           PortFolio
         </Link>
@@ -39,6 +47,63 @@ function NavBar() {
         <img src={contact} alt="" className="imageButtonContact" />
         Me contacter
       </Link>
+
+      <img
+        src={menu}
+        alt="menu"
+        className="navMenu"
+        onClick={() => {
+          setShowMenu(!showMenu);
+        }}
+      />
+      <div
+        className="mobileMenu"
+        style={{ display: showMenu ? "flex" : "none" }}
+      >
+        <Link
+          activeClass="active"
+          className="listItemMobileMenu"
+          to="presentation"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          onClick={() => setShowMenu(false)}
+        >
+          Accueil
+        </Link>
+        <Link
+          activeClass="active"
+          className="listItemMobileMenu"
+          to="a-propos"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          onClick={() => setShowMenu(false)}
+        >
+          À propos
+        </Link>
+        <Link
+          activeClass="active"
+          className="listItemMobileMenu"
+          to="portfolio"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          onClick={() => setShowMenu(false)}
+        >
+          PortFolio
+        </Link>
+        <Link
+          activeClass="active"
+          to="contact"
+          className="listItemMobileMenu"
+          spy={true}
+          smooth={true}
+          onClick={() => setShowMenu(false)}
+        >
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 }
